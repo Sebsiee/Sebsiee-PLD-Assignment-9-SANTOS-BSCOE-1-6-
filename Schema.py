b362fa63945fcbe064d77541d81d0b291a2973f3
Sebastian <santosbasty4@gmail.com>
2022-02-11 22:28:19 +0800

    Basis

---
+++ Schema.py
@@ -0,0 +1,86 @@
+{
+  "basics": {
+    "name": "Sebastian O. Santos",
+    "label": "Programmer",
+    "image": "https://scontent.fmnl17-1.fna.fbcdn.net/v/t39.30808-6/247468139_4558542057521964_8156934972956690701_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeFYeN2_x23DRFCFmuXXrMrEikQdzGIR_USKRB3MYhH9RIsvtoCvNjXJDcgs-OZ7HMcxBMpWXp7Mnr1QgAmk8Htj&_nc_ohc=IimgqIeIpYcAX-9nzSa&_nc_ht=scontent.fmnl17-1.fna&oh=00_AT9ewzm12V07kCahtmB3InKFfPsWX-u5ayoW2dDjRry0JQ&oe=620A890D",
+    "email": "santosbasty4@gmail.com",
+    "phone": "(+63) 956-819-6759",
+    "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    "summary": "Philantrophist | Entrepreneur | Inventor | Founder of the Internet",
+    "location": {
+      "address": "26 Lily Street, Area 9, Luzon Ave., Pasong Tamo",
+      "postalCode": "1117",
+      "city": "Quezon City",
+      "countryCode": "Ph",
+      "region": "Metro Manila"
+    },
+    "profiles": [{
+      "network": "Facebook",
+      "username": "Sebastian Santos",
+      "url": "https://www.facebook.com/sebsiee/"
+    }]
+  },
+  "work": [{
+    "name": "Meta",
+    "position": "CEO",
+    "url": "https://about.facebook.com/",
+    "startDate": "2022-02-05",
+    "endDate": "2022-02-11",
+    "summary": "Company that made a website which allows users, who sign-up for free profiles, to connect with friends, work colleagues or people they don't know, online. It allows users to share pictures, music, videos, and articles, as well as their own thoughts and opinions with however many people they like.",
+    "highlights": [
+      "Founded Facebook"
+    ]
+  }],
+  "volunteer": [{
+    "organization": "NASA",
+    "position": "Intern",
+    "url": "https://www.nasa.gov/",
+    "startDate": "2019-01-11",
+    "endDate": "2019-02-01",
+    "summary": "Responsible for unique scientific and technological achievements in human spaceflight, aeronautics, space science, and space applications that have had widespread impacts on our nation and the world.",
+    "highlights": [
+      "Awarded 'Intern of the Month'"
+    ]
+  }],
+  "education": [{
+    "institution": "Harvard University",
+    "url": "https://harvard.edu/",
+    "area": "Computer Engineering",
+    "studyType": "Bachelor",
+    "startDate": "2016-07-30",
+    "endDate": "2021-05-13",
+    "score": "5.0",
+    "courses": [
+      "Physics 16: Mechanics and Special Relativity",
+      "Chemistry 30: Organic Chemistry",
+      "Math 55a: Honors Abstract Algebra.",
+      "ES181: Engineering Thermodynamics"
+    ]
+  }],
+  "awards": [{
+    "title": "Nobel Prize",
+    "date": "2021-12-10",
+    "awarder": "Royal Swedish Academy of Sciences",
+    "summary": "Awarded for the most influential face."
+  }],
+  "skills": [{
+    "name": "Web Development",
+    "level": "Master",
+    "keywords": [
+      "HTML",
+      "CSS",
+      "Python"
+    ]
+  }],
+  "languages": [{
+    "language": "English",
+    "fluency": "Master"
+  }],
+  "interests": [{
+    "name": "Computer",
+    "keywords": [
+      "Hardwares",
+      "Softwares"
+    ]
+  }]
+}
